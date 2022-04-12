97d5cf9eccb5f42ad8eafe7466f7b0999b442b67
MushuDG <theo.digiacomo@gmail.com>
2022-04-12 14:10:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ fi
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install git zsh wget curl -y
-chsh /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
