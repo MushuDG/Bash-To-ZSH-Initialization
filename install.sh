cdefa711107d3bb643d4b28fc016d9ee9314c56e
MushuDG <theo.digiacomo@gmail.com>
2022-04-20 13:39:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt install git zsh wget curl neofetch -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 rm -rf ~/.zshrc
 rm -rf ~/.p10k.zsh
 cd ~
