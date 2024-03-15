2e107abd3cc173abe10df91a04e3b647d8972a68
MushuDG <theo.digiacomo@gmail.com>
2024-03-15 16:10:22 +0100

    🐛 FIX:  Progress bar

--- arch-based-install.sh
+++ arch-based-install.sh
@@ -15,6 +15,22 @@ spinner() {
     printf "    \b\b\b\b"
 }
 
+# Animated progress bar function
+progress_bar() {
+    local duration=$1
+    local progress=0
+    local progress_char="▋"
+    local total_char=30
+    local increment=$(( duration / total_char ))
+    printf " ["
+    while (( progress < duration )); do
+        printf "%s" "$progress_char"
+        ((progress += increment))
+        sleep 1
+    done
+    printf "]"
+}
+
 # Fancy hacker interface
 echo "Initializing installation..."
 sleep 1
@@ -32,19 +48,19 @@ fi
 # Update and upgrade packages
 echo -ne " [                    ] Updating packages..."
 sudo pacman -Syu --noconfirm > /dev/null 2>&1 &
-spinner
+progress_bar 10
 echo -ne " [✓] Updating packages... Done\n"
 
 # Install necessary packages
 echo -ne " [                    ] Installing packages..."
 sudo pacman -S --noconfirm git zsh wget curl neofetch bat thefuck > /dev/null 2>&1 &
-spinner
+progress_bar 10
 echo -ne " [✓] Installing packages... Done\n"
 
 # Install Oh My Zsh
 echo -ne " [                    ] Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" </dev/null > /dev/null 2>&1 &
-spinner
+progress_bar 10
 echo -ne " [✓] Installing Oh My Zsh... Done\n"
 
 # Check if Oh My Zsh installation succeeded
@@ -56,15 +72,15 @@ fi
 # Clone powerlevel10k, zsh-autosuggestions, zsh-you-should-use, zsh-bat and zsh-syntax-highlighting
 echo -ne " [                    ] Cloning plugins..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null 2>&1 &
-spinner
+progress_bar 10
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null 2>&1 &
-spinner
+progress_bar 10
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use > /dev/null 2>&1 &
-spinner
+progress_bar 10
 git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat > /dev/null 2>&1 &
-spinner
+progress_bar 10
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null 2>&1 &
-spinner
+progress_bar 10
 echo -ne " [✓] Cloning plugins... Done\n"
 
 # Remove existing zsh configuration files
@@ -74,16 +90,16 @@ echo -ne " [✓] Cloning plugins... Done\n"
 # Download new zsh configuration files
 echo -ne " [                    ] Downloading configuration files..."
 wget https://raw.githubusercontent.com/MushuDG/Bash-To-ZSH-Initialization/main/.p10k.zsh -O ~/.p10k.zsh > /dev/null 2>&1 &
-spinner
+progress_bar 10
 wget https://raw.githubusercontent.com/MushuDG/Bash-To-ZSH-Initialization/main/.zshrc -O ~/.zshrc > /dev/null 2>&1 &
-spinner
+progress_bar 10
 echo -ne " [✓] Downloading configuration files... Done\n"
 
 # Clean up
 echo -ne " [                    ] Cleaning up..."
 cd ..
 rm -rf ./Bash-To-ZSH-Initialization > /dev/null 2>&1 &
-spinner
+progress_bar 10
 echo -ne " [✓] Cleaning up... Done\n"
 
 # Set Zsh as the default shell if the user agrees
